b2c22011326bd0a2f088af8e87b2349810cf9c3d
Genokrad <zeeper90@gmail.com>
2023-02-22 20:17:20 +0200

    cocktails

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('hinpm start');
+import './js/open_modal';
+import './js/favorite-menu';
+import './js/search_header';
+import './js/favorit-coctails';
+import './js/favorite-ingridients';
+import './js/switch-theme';
+import './js/fetch';
+import './js/random-default-coctails';
+import './js/pagination';
+import './js/hero-select-m';
+import './js/hero';
+import './js/cocktails';
+import './js/localStorage';
+import './js/modalOpenClose';
+import './js/modal';
